21e25312b70e3c1ef4c65cb44a32a0caa296280c
KayedObeidat <KAY20180943@std.psut.edu.jo>
2020-10-29 16:43:31 +0300

    agile-ticketing-system

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Welcome')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Welcome')
+})
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
